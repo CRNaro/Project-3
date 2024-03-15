a0be713fba966337cdfb094ce175d6be6611d43e
baldwisj <baldwisj@gmail.com>
2024-03-15 12:03:44 -0400

    Set up File Structure and added intial code

---
+++ client/src/main.jsx
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App.jsx'
+//--import pages here--
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, 
+    //--ADD links to pages here--
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
