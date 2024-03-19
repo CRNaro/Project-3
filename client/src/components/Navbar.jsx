d230ed201bcdec18790b8cc5cde23308869f7be6
baldwisj <baldwisj@gmail.com>
2024-03-18 21:40:41 -0400

    Added login pop up

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,50 @@
-
+import { useState } from 'react';
 import React from 'react';
-import { Container, Row, Col, Card, Navbar, Nav } from 'react-bootstrap';
+import { Container, Row, Col, Card, Navbar, Nav, Modal, Tab } from 'react-bootstrap';
+import LoginForm from './LoginForm';
 
 function NavBar() {
+  const [showModal, setShowModal] = useState(false);
   return (
-    <Navbar bg="light">
+    <>
+      <Navbar bg="light">
         <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
+
+            <Nav.Link onClick={() => setShowModal(true)}>Login</Nav.Link>
+          </Nav>
         </Container>
-    </Navbar>   
+      </Navbar>
+      <Modal
+        size='lg'
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby='signup-modal'>
+        {/* tab container to do either signup or login component */}
+        <Tab.Container defaultActiveKey='login'>
+          <Modal.Header closeButton>
+            <Modal.Title id='signup-modal'>
+              <Nav variant='pills'>
+                <Nav.Item>
+                  <Nav.Link eventKey='login'>Login</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey='login'>
+                <LoginForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
+      </Modal>
+    </>
   );
 }
 
