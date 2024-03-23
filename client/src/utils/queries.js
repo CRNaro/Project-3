b9e4c197f300e6ce5de32c009fbdf03490b27207
Corri Naglie <csnaglie@hotmail.com>
2024-03-23 17:50:02 -0400

    adding missed me query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,35 +1,43 @@
 import { gql } from '@apollo/client';
 // This provides a query to grab the users info/saved material
+export const GET_ME = gql`
+query me {
+    me {
+      _id
+        }
+    }
+  `
+  
 export const customerInfo = gql`
   query customerInfo($email: String, $lastName: String) {
-    customerInfo(email: $email, lastName: $lastName) {
-      firstName
-      lastName
-      phoneNumber
-      email
-      customerNotes
-      _id
+  customerInfo(email: $email, lastName: $lastName) {
+    firstName
+    lastName
+    phoneNumber
+    email
+    customerNotes
+    _id
       products {
-        _id
-        manufacturer
-        serialNumber
-        modelNumber
-        installDate
-        warrantyDuration
-        cost
-        manual
-        installationNotes
-        installedBy
-      }
+      _id
+      manufacturer
+      serialNumber
+      modelNumber
+      installDate
+      warrantyDuration
+      cost
+      manual
+      installationNotes
+      installedBy
+    }
       parts {
-        _id
-        name
-        partNumber
-        cost
-        installDate
-        warrantyDuration
-        installedBy
-      }
+      _id
+      name
+      partNumber
+      cost
+      installDate
+      warrantyDuration
+      installedBy
     }
   }
+}
 `
