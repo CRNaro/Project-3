f203afb1af0603a39f591c1edd0e9f617451c107
baldwisj <baldwisj@gmail.com>
2024-03-25 19:16:26 -0400

    Added readme and adjusted home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
-
-import { Container, Form, Button, Row, Col, Card } from 'react-bootstrap';
-import storePic from '../assets/StorePic.jpg';
+import { Container, Row, Card } from 'react-bootstrap';
+import storePic from '../assets/storePic.jpg';
 import "../styles/Home.css";
 import "../styles/Customer.css";
 import "../styles/Footer.css";
-
-
-import Auth from '../utils/auth';
-
 
 function Home() {
   return (
@@ -19,23 +13,22 @@ function Home() {
       <NavBar />
       <Container fluid className="flex-grow-1 d-flex flex-column m-3">
         <Row className="flex-grow-1" >
-          
-                <Card className="home-card">
-                    <Card.Body>
-                        <Card.Title>Please Log In to Work With Customer Info</Card.Title>
-                        <Card.Text>
-                            Welcome to the home page. This is a simple example of a home page.
-                        </Card.Text>
-                      <div className="img-container">
-                  <Card.Img variant="top" src={storePic} className="card-image w-100" />
-                  </div>
-                    </Card.Body>
-                </Card>
-           
+          <Card className="home-card">
+            <Card.Body>
+              <Card.Title>Please Log In to Work With Customer Info</Card.Title>
+              <Card.Text>
+                Welcome to the home page. This is a simple example of a home page.
+              </Card.Text>
+              <div className="img-container">
+                <Card.Img variant="top" src={storePic} className="card-image w-100" />
+              </div>
+            </Card.Body>
+          </Card>
         </Row>
       </Container>
-      <Footer/>
+      <Footer />
     </div>
   );
-  }
+}
+
 export default Home;
